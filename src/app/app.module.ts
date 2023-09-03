2208e6e0875f2cf64e925364c96a6732cbe450e3
Sara Guerrero <saramguerrero234@gmail.com>
2023-09-03 18:01:28 -0500

    first files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { RoutingModule } from './routing/routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/routing/routing-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PresentationComponent } from '../presentation/presentation.component';
+import { ProjectsComponent } from '../projects/projects.component';
+
+const routes: Routes = [
+  {path: '', component: PresentationComponent},
+  {path: 'projects', component: ProjectsComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class RoutingRoutingModule { }
